299052e6224e9ab545973aaf077a2d7f74703335
joshua.merrell <joshua.merrell@inventive.io>
2026-06-30 08:22:11 -0500

    fix port

--- server.js
+++ server.js
@@ -388,7 +388,7 @@ process.on('SIGINT', () => {
   process.exit()
 })
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT || 3002
 app.listen(PORT, () => {
   console.log(`D2 watch server running on http://localhost:${PORT}`)
 })
