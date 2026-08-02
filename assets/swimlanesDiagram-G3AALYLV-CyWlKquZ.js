7897ab9b840e594744825af1b359b1a200c28d1e
brantmerrell <brantmerrell@users.noreply.github.com>
2026-08-02 19:35:38 +0000

    deploy: c3c0cbf346477ed67c7836fee587943375289385

---
+++ assets/swimlanesDiagram-G3AALYLV-CyWlKquZ.js
@@ -0,0 +1,8 @@
+import{c as e,s as r}from"./flowDiagram-23GEKE2U-CsUhpHYJ.js";import{_ as a}from"./index-BYErqIWh.js";import"./chunk-5VM5RSS4-fNTkNhMo.js";import"./chunk-XXDRQBXY-fs2lDEfl.js";import"./chunk-VR4S4FIN-DnBtUzmT.js";import"./chunk-32BRIVSS-Gppw7-iq.js";import"./channel-fTcK2XWW.js";var o=a(t=>`${r(t)}
+  .swimlane.cluster rect {
+    stroke: ${t.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,"getStyles"),s=o,f=e({defaultLayout:"swimlane",styles:s});export{f as diagram};
